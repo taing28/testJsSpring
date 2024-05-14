7bf6af87f153fcfe04c21f916ba3d16f85f4356b
taing28073 <tainghe171475@fpt.edu.vn>
2024-05-14 10:42:06 +0700

    Initial commit

---
+++ src/main/java/com/example/spring/TestJsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJsSpringApplication.class, args);
+    }
+
+}
